4430235b42d46d631da9c9f714f30ce98e012d5c
alisonsunn <alison.jyue.sun@gmail.com>
2024-03-15 00:37:22 +1000

    bubble

---
+++ js/bubble.js
@@ -0,0 +1,7 @@
+const box1 = document.querySelector(".box1")
+const box2 = document.querySelector(".box2")
+const box3 = document.querySelector(".box3")
+
+document.addEventListener("click", (e) => {
+  console.log(e.target);
+})
